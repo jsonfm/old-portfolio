921d1ed175bb225d691df7701e699ef1bc5afab4
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-03-08 18:18:36 -0500

    projects view updated

--- src/components/CardProject/index.js
+++ src/components/CardProject/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsFillEyeFill, BsGithub } from "react-icons/bs";
 import "./styles.scss";
 
 
@@ -6,17 +7,24 @@ const CardProject = () => {
     return(
         <div className="card-container">
             <div className="card-image-container">
-                <img className="card-image" src="https://lh3.googleusercontent.com/Bay2gwi4ZMDmZ3lbvs4Mv15mEKJkpQbwmBQxwzrI7NncvtCsTOiyM1psv1KYjQsgz3cVl4PY9H85x_Oh33ctrdtJdVo=w640-h400-e365-rj-sc0x00ffffff"/>
+                <img className="card-image" src="https://lh3.googleusercontent.com/Bay2gwi4ZMDmZ3lbvs4Mv15mEKJkpQbwmBQxwzrI7NncvtCsTOiyM1psv1KYjQsgz3cVl4PY9H85x_Oh33ctrdtJdVo=w640-h400-e365-rj-sc0x00ffffff" alt="image-project"/>
             </div>
             <div className="card-content-container">
                 <div className="card-text">
+                    <h3 className="card-title text-center">Project Title</h3>
                     <p className="truncated">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
                 </div>
                 <div className="card-links">
-                    <div className="github-button"> </div>
-                    <div className="demo-button"></div>
+                    <div className="card-button github-button">
+                        <BsGithub />
+                        <p>Source</p>
+                    </div>
+                    <div className="card-button demo-button">
+                        <BsFillEyeFill/>
+                        <p>Demo</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,12 +11,24 @@ const ProjectsPage = () =>{
             <SEO
                 title="Projects"
             />            
-            <section class="projects-section d-flex flex-column v-center h-center">
+            <section class="projects-section">
                 <h2 class="text-center mt-2">Here you can check some projects I've worked.</h2>
                 <hr class="projects-hr"/>
+
                 <div class="projects-grid">
-                    <CardProject />
+                    <div class="projects-row">
+                        <div class="projects-col">
+                            <CardProject />
+                        </div>
+                        <div class="projects-col">
+                            <CardProject />
+                        </div>
+                        <div class="projects-col">
+                            <CardProject />
+                        </div>
+                    </div>
                 </div>
+
             </section>
         </Layout>
     );
