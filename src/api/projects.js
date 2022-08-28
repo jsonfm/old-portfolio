7194084fd3144ae621f1a5c9236b597eb29dc420
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-08-28 15:23:09 -0500

    username repo updated

--- src/api/projects.js
+++ src/api/projects.js
@@ -2,65 +2,65 @@ const projects = [
     {
         "name": "Radiance",
         "description": "A stock images app design made with HTML, CSS and JS.",
-        "image": "https://github.com/Hikki12/radiance/blob/master/docs/images/radiance.jpg?raw=true",
-        "repository": "https://github.com/Hikki12/radiance",
-        "demo": "https://hikki12.github.io/radiance/"
+        "image": "https://github.com/jsonfm/radiance/blob/master/docs/images/radiance.jpg?raw=true",
+        "repository": "https://github.com/jsonfm/radiance",
+        "demo": "https://jsonfm.github.io/radiance/"
     },
     {
         "name": "Resto",
         "description": "A restaurant landing page made with HTML, CSS and JS.",
-        "image": "https://github.com/Hikki12/resto/blob/master/docs/images/resto.jpeg?raw=true",
-        "repository": "https://github.com/Hikki12/resto",
-        "demo": "https://hikki12.github.io/resto/"
+        "image": "https://github.com/jsonfm/resto/blob/master/docs/images/resto.jpeg?raw=true",
+        "repository": "https://github.com/jsonfm/resto",
+        "demo": "https://jsonfm.github.io/resto/"
     },
     {
         "name": "Fruitopia",
         "description": "A landing page design called fruitopia which is a fruits store. It was made with HTML, CSS and JS.",
-        "image": "https://github.com/Hikki12/fruitopia/blob/master/docs/images/fruitopia.jpg?raw=true",
-        "repository": "https://github.com/Hikki12/fruitopia",
-        "demo": "https://hikki12.github.io/fruitopia/"
+        "image": "https://github.com/jsonfm/fruitopia/blob/master/docs/images/fruitopia.jpg?raw=true",
+        "repository": "https://github.com/jsonfm/fruitopia",
+        "demo": "https://jsonfm.github.io/fruitopia/"
     },
     {
         "name": "Underwater",
         "description": "A html landing page with cool video backgrounds related with the sea. Made with HTML, CSS and JS.",
-        "image": "https://github.com/Hikki12/underwater/blob/master/docs/images/underwater.png?raw=true",
-        "repository": "https://github.com/Hikki12/underwater",
-        "demo": "https://hikki12.github.io/underwater/"
+        "image": "https://github.com/jsonfm/underwater/blob/master/docs/images/underwater.png?raw=true",
+        "repository": "https://github.com/jsonfm/underwater",
+        "demo": "https://jsonfm.github.io/underwater/"
     },
     {
         "name": "Food.",
         "description": "🍖 😋 A restaurant website as a landing page made with HTML, CSS and JS.",
-        "image": "https://github.com/Hikki12/food/blob/master/docs/images/food.png?raw=true",
-        "repository": "https://github.com/Hikki12/food",
-        "demo": "https://hikki12.github.io/food/"
+        "image": "https://github.com/jsonfm/food/blob/master/docs/images/food.png?raw=true",
+        "repository": "https://github.com/jsonfm/food",
+        "demo": "https://jsonfm.github.io/food/"
     },
     {
         "name": "Xypo Site",
         "description": "A landing page for an e-learning app called Xypo, made with HTML, CSS and JS.",
-        "image": "https://github.com/Hikki12/xypo-website/blob/master/docs/images/xyposite.png?raw=true",
-        "repository": "https://github.com/Hikki12/xypo-website",
-        "demo": "https://hikki12.github.io/xypo-website/"
+        "image": "https://github.com/jsonfm/xypo-website/blob/master/docs/images/xyposite.png?raw=true",
+        "repository": "https://github.com/jsonfm/xypo-website",
+        "demo": "https://jsonfm.github.io/xypo-website/"
     },
     {
         "name": "Smartwatch landing Page",
         "description": "A product responsive website made with HTML, CSS and JS.",
-        "image": "https://github.com/Hikki12/smartwatch-landing-page/blob/master/docs/images/smartwatch.png?raw=true",
-        "repository": "https://github.com/Hikki12/smartwatch-landing-page",
-        "demo": "https://hikki12.github.io/smartwatch-landing-page/"
+        "image": "https://github.com/jsonfm/smartwatch-landing-page/blob/master/docs/images/smartwatch.png?raw=true",
+        "repository": "https://github.com/jsonfm/smartwatch-landing-page",
+        "demo": "https://jsonfm.github.io/smartwatch-landing-page/"
     },
     {
         "name": "Movies search App",
         "description": "An svelte App for searching movies from the omdb API.",
-        "image": "https://raw.githubusercontent.com/Hikki12/search-movies-app/master/docs/images/searchmoviesapp.jpeg",
-        "repository": "https://github.com/Hikki12/search-movies-app",
-        "demo": "https://hikki12.github.io/search-movies-app/",
+        "image": "https://raw.githubusercontent.com/jsonfm/search-movies-app/master/docs/images/searchmoviesapp.jpeg",
+        "repository": "https://github.com/jsonfm/search-movies-app",
+        "demo": "https://jsonfm.github.io/search-movies-app/",
     },
     {
         "name": "Platzi Travel App",
         "description": "A frontend project made with TailwindCSS and some JS.",
-        "image": "https://github.com/Hikki12/platzi-travel/raw/master/design/platzitravelapp.png",
-        "repository": "https://github.com/Hikki12/platzi-travel",
-        "demo": "https://hikki12.github.io/platzi-travel/",
+        "image": "https://github.com/jsonfm/platzi-travel/raw/master/design/platzitravelapp.png",
+        "repository": "https://github.com/jsonfm/platzi-travel",
+        "demo": "https://jsonfm.github.io/platzi-travel/",
     },
     {
         "name": "CAMIE",
@@ -71,82 +71,82 @@ const projects = [
     {
         "name": "REMIO",
         "description": "Some time ago, I worked on the development of remote labs at my university. As a result of this work, I generated useful tools for the development of IOT devices in real time.",
-        "image": "https://hikki12.github.io/remio/assets/images/arch-1.png",
-        "repository": "https://github.com/Hikki12/remio",
-        "demo": "https://hikki12.github.io/remio/"
+        "image": "https://jsonfm.github.io/remio/assets/images/arch-1.png",
+        "repository": "https://github.com/jsonfm/remio",
+        "demo": "https://jsonfm.github.io/remio/"
     },
     {
         "name": "REMIO-LAB",
         "description": "I created a simple remote laboratory with python and django, open source, for everybody who wants to create them own experiments.",
-        "image": "https://github.com/Hikki12/remiolab/blob/master/remiolab/static/images/remiolab.png?raw=true",
-        "repository": "https://github.com/Hikki12/remiolab",
+        "image": "https://github.com/jsonfm/remiolab/blob/master/remiolab/static/images/remiolab.png?raw=true",
+        "repository": "https://github.com/jsonfm/remiolab",
     },
     {
         "name": "Sinculpa Restaurant Menu",
         "description": "I made this project for a restaurant in my city, I used GatsbyJS and Tailwind CSS, and I deployed it in github pages.",
         "image": "https://sinculpasite.files.wordpress.com/2022/07/sinculpa.png",
-        "repository": "https://github.com/Hikki12/sinculpa",
-        "demo": "https://hikki12.github.io/sinculpa/"
+        "repository": "https://github.com/jsonfm/sinculpa",
+        "demo": "https://jsonfm.github.io/sinculpa/"
     },
     {
         "name": "Platzi Avo Store",
         "description": "An Avocates store made with NextJS an deployed on vercel.",
         "image": "https://static.platzi.com/media/porfilio/platziavostore_949f39e9-3d1f-4314-9e31-388ebe198a42.png",
-        "repository": "https://github.com/Hikki12/platzi-avos-app",
+        "repository": "https://github.com/jsonfm/platzi-avos-app",
         "demo": "https://platzi-avos-app.vercel.app/"
     },
     {
         "name": "Gluzy site",
         "description": "This is a page prototype of a personal project I have with some friends, which consists of a medical device for measure blood glucose level on diabetic people.",
-        "image": "https://github.com/Hikki12/gluzy-site/blob/master/docs/images/gluzysite.png?raw=true",
-        "repository": "https://github.com/Hikki12/gluzy-site",
-        "demo": "https://hikki12.github.io/gluzy-site/"
+        "image": "https://github.com/jsonfm/gluzy-site/blob/master/docs/images/gluzysite.png?raw=true",
+        "repository": "https://github.com/jsonfm/gluzy-site",
+        "demo": "https://jsonfm.github.io/gluzy-site/"
     },
     {
         "name": "React Store",
         "description": "This is a store made with ReactJS. It uses React context, actions, and reducers. It was written on Typescript.",
-        "image": "https://github.com/Hikki12/react-store/blob/master/docs/images/reactstore.png?raw=true",
-        "repository": "https://github.com/Hikki12/react-store",
-        "demo": "https://hikki12.github.io/react-store/"
+        "image": "https://github.com/jsonfm/react-store/blob/master/docs/images/reactstore.png?raw=true",
+        "repository": "https://github.com/jsonfm/react-store",
+        "demo": "https://jsonfm.github.io/react-store/"
     },
     {
         "name": "Python Oscilloscope",
         "description": "I used to work with Arduino in some projects, so I decided to do an oscilloscope. I made it with Python and PyQt5 Framework.",
-        "image": "https://github.com/Hikki12/oscilloscope-python-arduino/raw/master/screenshots/ss1.png",
-        "repository": "https://github.com/Hikki12/oscilloscope-python-arduino",
+        "image": "https://github.com/jsonfm/oscilloscope-python-arduino/raw/master/screenshots/ss1.png",
+        "repository": "https://github.com/jsonfm/oscilloscope-python-arduino",
     },
     {
         "name": "REMIO Experiment",
         "description": "An example of experiment using my remio library, PyQt5 and Arduino.",
-        "image": "https://github.com/Hikki12/remio-mockup-template/raw/master/docs/gui.png",
-        "repository": "https://github.com/Hikki12/remio-mockup-template",
+        "image": "https://github.com/jsonfm/remio-mockup-template/raw/master/docs/gui.png",
+        "repository": "https://github.com/jsonfm/remio-mockup-template",
     },
     {
         "name": "Time tracking dashboard",
         "description": "This is a frontend mentor challenge called time tracking board, which I developed as a hands-on exercise using HTML, CSS, and JS.",
-        "image": "https://github.com/Hikki12/time-tracking-dashboard-challenge/raw/master/design/desktop-preview.jpg",
-        "repository": "https://github.com/Hikki12/time-tracking-dashboard-challenge/",
-        "demo": "https://hikki12.github.io/time-tracking-dashboard-challenge/"
+        "image": "https://github.com/jsonfm/time-tracking-dashboard-challenge/raw/master/design/desktop-preview.jpg",
+        "repository": "https://github.com/jsonfm/time-tracking-dashboard-challenge/",
+        "demo": "https://jsonfm.github.io/time-tracking-dashboard-challenge/"
     },
     {
         "name": "loopstudio-challenge",
         "description": "This is a frontend mentor challenge called Loopstudios landing page, which I developed as a hands-on exercise using HTML, CSS, and JS.",
-        "image": "https://github.com/Hikki12/loopstudio-challenge/raw/master/design/desktop-preview.jpg",
-        "repository": "https://github.com/Hikki12/loopstudio-challenge",
-        "demo": "https://hikki12.github.io/loopstudio-challenge/",
+        "image": "https://github.com/jsonfm/loopstudio-challenge/raw/master/design/desktop-preview.jpg",
+        "repository": "https://github.com/jsonfm/loopstudio-challenge",
+        "demo": "https://jsonfm.github.io/loopstudio-challenge/",
     },
     {
         "name": "Batata Studio",
         "description": "This is an simple example of HTML, CSS, JS app, I made some time ago.",
-        "image": "https://github.com/Hikki12/batata-app-html/blob/master/assets/img/bitcoinbaby2x.jpg?raw=true",
-        "repository": "https://github.com/Hikki12/batata-app-html",
-        "demo": "https://hikki12.github.io/batata-app-html/",
+        "image": "https://github.com/jsonfm/batata-app-html/blob/master/assets/img/bitcoinbaby2x.jpg?raw=true",
+        "repository": "https://github.com/jsonfm/batata-app-html",
+        "demo": "https://jsonfm.github.io/batata-app-html/",
     },
     {
         "name": "Golang MJPEG server",
         "description": "I implemented a simple MJPEG Server with Golang. I used Opencv working together with a http server.",
         "image": "https://images.unsplash.com/photo-1623949556303-b0d17d198863?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        "repository": "https://github.com/Hikki12/go-mjpeg-server",
+        "repository": "https://github.com/jsonfm/go-mjpeg-server",
     },
 ];
 
