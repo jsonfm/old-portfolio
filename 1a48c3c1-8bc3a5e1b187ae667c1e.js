2462610f1bd3092a94947125767bbe4f91a08227
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-09-05 13:47:51 -0500

    Updates

---
+++ 1a48c3c1-8bc3a5e1b187ae667c1e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[874],{7244:function(t,c,r){r.d(c,{db5:function(){return e}});var a=r(1046);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M19 2H5c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h3.586L12 21.414 15.414 18H19c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zm0 14h-4.414L12 18.586 9.414 16H5V4h14v12z"}},{tag:"circle",attr:{cx:"15",cy:"10",r:"2"}},{tag:"circle",attr:{cx:"9",cy:"10",r:"2"}}]})(t)}}}]);
+//# sourceMappingURL=1a48c3c1-8bc3a5e1b187ae667c1e.js.map
